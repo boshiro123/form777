1eea156425247cd13266c9cc49511e4a84bcfbeb
boshiro123 <90728621+boshiro123@users.noreply.github.com>
2021-10-12 18:16:34 +0300

    task2(trpo)

---
+++ vecherdobriy/vecherdobriy/Source.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Добрый вечер, Марина Михайловна, извиниться что домашка в последний момент";
+	return 0;
+}
